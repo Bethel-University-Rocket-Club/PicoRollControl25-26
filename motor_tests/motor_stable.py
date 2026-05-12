9f2a9c73c136a01aa4439dd038d3517486f09e5c
David Schieffer <Gdaveonline@gmail.com>
2026-05-11 20:37:03 -0500

    updated motor_stable test for cumulative motor speed setting

--- motor_tests/motor_stable.py
+++ motor_tests/motor_stable.py
@@ -6,14 +6,6 @@ import utime
 
 gyro = None
 
-LOOP_SPEED = 0.01
-
-CLOCKWISE_SPEED = 5000
-COUNTERCLOCKWISE_SPEED = -5000
-NO_SPEED = 0
-ACTIVE_ROLL_START_THRESHOLD = 10
-ACTIVE_ROLL_STOP_THRESHOLD = 2
-
 def motor_setup():
     # Configure motor
     motor_controller.motor_I2C_bus = I2C(0, scl=Pin(5), sda=Pin(4))
@@ -21,7 +13,7 @@ def motor_setup():
     
     motor_controller.init_motor()
     
-    motor_controller.set_timeout_time(500) #ms
+    motor_controller.set_timeout_time(300) #ms
     
     #as fast as possible
     motor_controller.set_max_acceleration(1, 0)
@@ -41,33 +33,32 @@ def gyro_setup():
     time.sleep(0.5)
     #gyro.calibration_test(0, 0)
     
-cumAngle = 0
+def get_motor_speed_from_roll(roll):
+    return -0.004 * roll
+    
 def motor_stable():
-    global gyro, LOOP_SPEED, cumAngle, CLOCKWISE_SPEED, COUNTERCLOCKWISE_SPEED, NO_SPEED, ACTIVE_ROLL_START_THRESHOLD, ACTIVE_ROLL_STOP_THRESHOLD
-    activeRolling = False
-    while True:
-        deltaTime = LOOP_SPEED
-        deltaRoll = gyro.get_gyroX()
-        cumAngle += deltaRoll * deltaTime
-        if cumAngle > ACTIVE_ROLL_START_THRESHOLD:
-            motor_controller.set_speed(1, COUNTERCLOCKWISE_SPEED)
-            activeRolling = True
-        elif cumAngle < -ACTIVE_ROLL_START_THRESHOLD:
-            motor_controller.set_speed(1, CLOCKWISE_SPEED)
-            activeRolling = True
-        else:
-            if activeRolling:
-                if cumAngle > ACTIVE_ROLL_STOP_THRESHOLD:
-                    motor_controller.set_speed(1, COUNTERCLOCKWISE_SPEED)
-                elif cumAngle < -ACTIVE_ROLL_STOP_THRESHOLD:
-                    motor_controller.set_speed(1, CLOCKWISE_SPEED)
-                else:
-                    activeRolling = False
-                    motor_controller.set_speed(1, NO_SPEED)
-            else:
-                motor_controller.set_speed(1, NO_SPEED)
-        time.sleep(LOOP_SPEED)
-                
+    global gyro
+    cum_motor_speed = 0
+    cum_roll = 0
+    start_time_ms = utime.ticks_ms()
+    cur_time_ms = utime.ticks_ms()
+    roll_check = -1
+    while utime.ticks_diff(cur_time_ms, start_time_ms) < 30000:
+        prev_time_ms = cur_time_ms
+        cur_time_ms = utime.ticks_ms()
+        delta_roll = gyro.get_gyroX()
+        delta_time = utime.ticks_diff(cur_time_ms, prev_time_ms)
+        cum_roll += delta_roll * delta_time / 1000
+        #every 250 milliseconds drive the motor - gives it time to adjust
+        if utime.ticks_diff(cur_time_ms, start_time_ms) // 250 > roll_check:
+            roll_check += 1
+            cum_motor_speed += get_motor_speed_from_roll(delta_roll)
+            #fastest direction to go to get back to 0
+            min_from_zero = (cum_roll % 360) - 180
+            cum_motor_speed += min_from_zero * -0.1 #return to 0
+            motor_controller.set_speed(1, cum_motor_speed)
+        #record data here
+
 motor_setup()
 gyro_setup()
 motor_stable()
